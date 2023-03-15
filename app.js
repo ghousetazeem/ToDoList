c43dc5fb218091c311007b5665d7e79f91310d02
Mohd Ghouse Tazeem <74200389+MohdGhouse-75@users.noreply.github.com>
2023-03-16 00:00:16 +0530

    Add CSS file

--- app.js
+++ app.js
@@ -6,27 +6,28 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
-var items = ["Buy Food", "Cook Food", "Eat Food"];
+let items = ["Buy Food", "Cook Food", "Eat Food"];
 
 app.get("/", function(req, res){
     // res.send("<h1>Everything is Running</h1>");
     const today = new Date();
-    var options = {
+    let options = {
         weekday: "long",
         day: "numeric",
         month: "long",
         year: "numeric"
     }
 
-    var day = today.toLocaleDateString("en-US", options);
+    let day = today.toLocaleDateString("en-US", options);
 
     res.render("list", {kindOfDay: day, addedItems: items});
 
 });
 
 app.post("/", function(req, res){
-    var item = req.body.toDo;
+    let item = req.body.toDo;
     items.push(item);
 
     res.redirect("/");
